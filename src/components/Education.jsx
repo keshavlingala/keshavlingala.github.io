897c83baaf8ab299f3ca8d3da1d54a0dadab1b59
Keshav Reddy <keshavlingala@gmail.com>
2023-06-01 00:07:35 -0700

    Projects Update

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,56 +1,56 @@
 import React from "react"
-import { Div, TimeLine, TimelineItem } from "./WorkExperience"
+import {Div, TimeLine, TimelineItem} from "./WorkExperience"
 
-const educations=[
-  {
-    key:'1',
-    name:'Keshav Memorial Institute of Technology',
-    course:'Computer Science and Engineering',
-    percentage:'7.61 CGPA',
-    description:'',
-    location:'Hyderabad, India',
-    date:{
-      startDate:'2017',
-      endDate:'2021'
+const educations = [
+    {
+        key: '1',
+        name: 'Keshav Memorial Institute of Technology',
+        course: 'Computer Science and Engineering',
+        percentage: '7.61 CGPA',
+        description: '',
+        location: 'Hyderabad, India',
+        date: {
+            startDate: '2017',
+            endDate: '2021'
+        }
+    },
+    {
+        key: '2',
+        name: 'California State University, Fullerton',
+        course: 'Master of Science in Computer Science',
+        percentage: '3.94/4 CGPA',
+        description: '',
+        location: 'Fullerton, California',
+        date: {
+            startDate: '2022',
+            endDate: 'Present'
+        }
     }
-  },
-  {
-    key:'2',
-    name:'California State University, Fullerton',
-    course:'Master of Science in Computer Science',
-    percentage:'',
-    description:'',
-    location:'Fullerton, California',
-    date:{
-      startDate:'2022',
-      endDate:'Present'
-    }
-  }
 ]
 
 const Education = () => {
-  return (
-    <TimeLine>
-      {educations.sort(o=>-o.date.startDate).map(edu=>{
-        return <TimelineItem key={edu.key}>
-          <Div>
-            <h3>
-              {edu.name}
-            </h3>
-            <h6>
-              {edu.course}
-            </h6>
-            <span className='location'>{edu.location}</span>
-            <span className='date'>{edu.date.startDate} - {edu.date.endDate}</span>
-            <p>
-              {edu.percentage}
-            </p>
-          </Div>
-        </TimelineItem>
-      })}
+    return (
+        <TimeLine>
+            {educations.sort(o => -o.date.startDate).map(edu => {
+                return <TimelineItem key={edu.key}>
+                    <Div>
+                        <h3>
+                            {edu.name}
+                        </h3>
+                        <h6>
+                            {edu.course}
+                        </h6>
+                        <span className='location'>{edu.location}</span>
+                        <span className='date'>{edu.date.startDate} - {edu.date.endDate}</span>
+                        <p>
+                            {edu.percentage}
+                        </p>
+                    </Div>
+                </TimelineItem>
+            })}
 
-    </TimeLine>
-  )
+        </TimeLine>
+    )
 }
 
 export default Education
