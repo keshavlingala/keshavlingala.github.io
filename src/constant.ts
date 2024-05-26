1ac26b2ddbc272b4958ce26588867f0473f8e111
Keshav <36675392+keshavlingala@users.noreply.github.com>
2024-05-25 22:47:03 -0400

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
-export const MAILING_URL = "https://mailing-service.up.railway.app/";
+export const MAILING_URL = "https://mailing-service.up.railway.app/keshav";
 // export const MAILING_URL = "http://localhost:3000/";
